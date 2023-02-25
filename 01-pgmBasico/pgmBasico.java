ff73f47c9a05a29b40fac3112136df9fc2950e30
santiago duran <santidurancanedo@gmail.com>
2023-02-24 20:49:32 -0500

    pgm basico 1

---
+++ 01-pgmBasico/pgmBasico.java
@@ -0,0 +1,6 @@
+public class pgmBasico{
+    public static void main(String[] args) {
+        System.out.println("||| PgmBasico 1 |||");
+        System.out.println("||| Author: Santiago Duran |||");
+    }
+}
