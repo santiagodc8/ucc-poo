18bb517555de87244bf24991a4b7fae44e95922a
santiago duran <santidurancanedo@gmail.com>
2023-03-10 15:08:43 -0500

    Add a class TipoCarga

--- 06_pgm_Avion/src/co/edu/campusucc/poo/MainAirships.java
+++ 06_pgm_Avion/src/co/edu/campusucc/poo/MainAirships.java
@@ -14,6 +14,22 @@ public class MainAirships {
         avion.landing();
         avion.stopingEngine();
 
+        avion = new TipoCarga();
+        avion.setMarca("Airbus");
+        avion.setTipo("Carga");
+        System.out.println(avion);
+
+        avion.startingEngine();
+        avion.speedUp();
+        avion.speedUp();
+        avion.currentSpeed();
+        avion.descent();
+        avion.speedUp();
+        avion.descent();
+        avion.currentSpeed();
+        avion.landing();
+        avion.stopingEngine();
+
     }
     
 }
