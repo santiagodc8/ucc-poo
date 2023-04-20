98304c7d3bec016a53b4a95f44950769937ec627
santiago duran <santidurancanedo@gmail.com>
2023-04-20 16:30:50 -0500

    add second part of reto python

---
+++ python/reto_python/3_control_flow/11_coin_flip.py
@@ -0,0 +1,9 @@
+import random
+
+num = random.randint(0, 1) 
+
+if num > 0.5:
+    print('Heads')
+else:
+    print('Tails')
+    
---
+++ python/reto_python/3_control_flow/12_grades.py
@@ -0,0 +1,8 @@
+import random
+
+grade = random.randint(0, 100)
+
+if grade >= 55:
+    print('Aprobó')
+else: 
+    print('Falló')    
