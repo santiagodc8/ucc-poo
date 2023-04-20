e2f09bbfaaf95a0c525f42eeb94f716d7873d76e
santiago duran <santidurancanedo@gmail.com>
2023-04-19 20:05:21 -0500

    add first part of the reto python

---
+++ python/reto_python/hello_word/04_initials.py
@@ -0,0 +1,9 @@
+# Mi nombre es Santiago Duran, soy de Valledupar, la tierra del Vallenato
+
+print('S S S S   D D D D D D')  
+print('S           D       D')
+print('S           D       D')
+print('S S S S     D       D')
+print('      S     D       D')
+print('      S     D       D')
+print('S S S S   D D D D D D')
